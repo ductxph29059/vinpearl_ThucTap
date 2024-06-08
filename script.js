adbf96f02088d8a51aa21f5dc162eb89ba093b00
ductxph29059 <137851512+ductxph29059-herry@users.noreply.github.com>
2024-06-08 11:38:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let navbar = document.querySelector(".navbar");
+
+// document.querySelector("#menu-btn").onclick = () => {
+//   navbar.classList.toggle("active");
+//   searchForm.classList.remove("active");
+//   cartItem.classList.remove("active");
+// };
+
+$(document).ready(function () {
+  $;
+  window.scroll(function () {
+    if ($(this).scrollTop()) {
+      $("header").addclass("sticky");
+    } else {
+      $("header").romew("sticky");
+    }
+  });
+});
